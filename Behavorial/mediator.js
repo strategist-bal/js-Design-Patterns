86151cb5423a7add24256968d3669aa90fd125a2
Baljeet Singh Dhanoa <baljeetdhanoa@Baljeets-MacBook-Air.local>
2023-05-18 23:42:12 +0530

    mediator design pattern example using a chatbot

--- Behavorial/mediator.js
+++ Behavorial/mediator.js
@@ -12,12 +12,45 @@ Participant.prototype = {
     }
 }
 
-var chatroom = function () {
+var Chatroom = function () {
     var participants = {};
 
     return {
-        register: function (participants) {
+        register: function (participant) {
+            participants[participants.name] = participant;
+            participant.chatroom = this;
+        },
 
+        send: function (message, from, to) {
+            if (to) {
+                to.receive(messsage, from);
+            } else {
+                for(key in participants) {
+                    if(participants[key] !== from) {
+                        participants[key].receive(message, from);
+                    }
+                }
+            }
         }
     }
 }
+
+function run() {
+    var baljeet = new Participant("Baljeet");
+    var friend = new Participant("friend");
+    var monAmi = new Participant("monAmi");
+    var meraDost = new Participant("meraDost");
+
+    var chatroom = new chatroom();
+    chatroom.register(baljeet);
+    chatroom.register(friend);
+    chatroom.register(monAmi);
+    chatroom.register(meraDost);
+
+    baljeet.send("Hello Guys");
+    friend.send("hey man");
+    monAmi.send("Long time",baljeet);
+    meraDost.send("Where have you been all this time?", friend);
+}
+
+run();
